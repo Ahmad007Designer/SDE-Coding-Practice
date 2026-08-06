23b5729f4a41dda8ea69d914d03c857493253cd2
Ahmad007Designer <ahmadmohdrock007@gmail.com>
2026-08-06 10:18:19 +0530

    better solution

---
+++ Array_part1/set-matrix-zeroes/better_case.cpp
@@ -0,0 +1,32 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0. You must do it in place.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    void setZeroes(vector<vector<int>>& matrix){
+        int m = matrix.size(); //length of row
+        int n= matrix[0].size();//length of cols
+        int row_arr[m]={0};
+        int col_row[n]={0};
+        for(int i=0;i<m; i++){
+            for(int j=0;j<n; j++){
+                
+            }
+        }
+        
+    }
+};
+int main(){
+    vector<vector<int>> matrix={{1,1,1},{1,0,1},{1,1,1}};
+    Solution sol;
+    sol.setZeroes(matrix);
+    for(auto row:matrix){
+        for(auto val: row){
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
