ae8261ed40937b3b55658cfc084fa7aee65ea453
Ahmad Husain <ahmadmohdrock007@gmail.com>
2026-08-10 09:10:22 +0530

    better_case

--- Array_part1/set-matrix-zeroes/better_case.cpp
+++ Array_part1/set-matrix-zeroes/better_case.cpp
@@ -9,10 +9,22 @@ public:
         int m = matrix.size(); //length of row
         int n= matrix[0].size();//length of cols
         int row_arr[m]={0};
-        int col_row[n]={0};
+        int col_arr[n]={0};
         for(int i=0;i<m; i++){
             for(int j=0;j<n; j++){
-                
+                if(matrix[i][j]==0){
+                    row_arr[i]=1;
+                    col_arr[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m; i++){
+            for(int j=0;j<n; j++){
+                if(row_arr[i]==1){
+                    if(matrix[i][j] !=0) matrix[i][j]=0;
+                }else if(col_arr[j]==1){
+                    if(matrix[i][j] !=0) matrix[i][j]=0;
+                }
             }
         }
         
